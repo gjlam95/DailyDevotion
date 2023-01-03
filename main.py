9a1c9676d5ba187147e93dd38700548fd7c7cc95
Daniel Lam <gj_lam@hotmail.com>
2023-01-03 13:23:02 +0800

    chore: keep bot alive

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import logging
 import urllib.parse
+from telegram import Update
 import urlfetch
 import re
 from bs4 import BeautifulSoup
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+from telegram.ext import CommandHandler, MessageHandler, Filters, ApplicationBuilder
 
-updater = Updater(os.environ.get('BOT_TOKEN'),
-                  use_context=True)
+# updater = Updater(os.environ.get('BOT_TOKEN'),
+#                   use_context=True)
+updater = ApplicationBuilder().token(os.environ.get('BOT_TOKEN')).build()
 
 EMPTY = 'empty'
 
